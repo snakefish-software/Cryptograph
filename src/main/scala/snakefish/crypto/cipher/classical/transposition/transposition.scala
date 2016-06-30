464f5467e734ad21f905c84cb2b9cb83d8d5b973
Dima Siryk <dmytro.siryk@gmail.com>
2016-06-30 19:02:07 +0300

    Move base columnar transposition logic to separate class

---
+++ src/main/scala/snakefish/crypto/cipher/classical/transposition/transposition.scala
@@ -0,0 +1,23 @@
+package snakefish.crypto
+package cipher.classical
+
+package object transposition {
+  
+  def normalizeKey(key: Array[Int], sameLetterAsNextInOrder: Boolean): Array[Int] = {
+    val res = new Array[Int](key.length)
+    var keySorted = key.clone.sorted
+    if (!sameLetterAsNextInOrder)
+      keySorted = keySorted.distinct
+    
+    for (i <- 0 until key.length) {
+      val index = keySorted.indexOf(key(i))
+      res(i) = index
+      
+      if (sameLetterAsNextInOrder) 
+        keySorted(index) = -1
+    }
+    
+    res
+  }
+  
+}
--- src/main/scala/snakefish/crypto/cipher/classical/transposition/СolumnarTransposition.scala
+++ src/main/scala/snakefish/crypto/cipher/classical/transposition/СolumnarTransposition.scala
@@ -52,23 +52,6 @@ abstract class СolumnarTransposition(key: CharSequence,
   protected def getLastRowLength(dataLength: Int): Int = 
     colsCount - (colsCount * getRowsCount(dataLength) - dataLength)
   
-  private def normalizeKey(key: Array[Int], sameLetterAsNextInOrder: Boolean): Array[Int] = {
-    val res = new Array[Int](key.length)
-    var keySorted = key.clone.sorted
-    if (!sameLetterAsNextInOrder)
-      keySorted = keySorted.distinct
-    
-    for (i <- 0 until key.length) {
-      val index = keySorted.indexOf(key(i))
-      res(i) = index
-      
-      if (sameLetterAsNextInOrder) 
-        keySorted(index) = -1
-    }
-    
-    res
-  }
-  
   private def createEmptyCols(colsHeight: Int): Array[StringBuilder] = {
     val cols = new Array[StringBuilder](colsCount)
     for (i <- 0 until colsCount) {
